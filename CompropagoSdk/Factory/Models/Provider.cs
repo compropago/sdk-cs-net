3c3d8a5599f529376d51cc0579e358137a03776d
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2017-02-01 16:09:41 -0600

    change returning types, remove auth and update payload provider

--- CompropagoSdk/Factory/Models/Provider.cs
+++ CompropagoSdk/Factory/Models/Provider.cs
@@ -4,6 +4,7 @@
     {
         public string name { get; set; }
         public string store_image { get; set; }
+        public string availability { get; set; }
         public bool is_active { get; set; }
         public string internal_name { get; set; }
         public string image_small { get; set; }
